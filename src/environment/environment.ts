59946dd1e10ce977581a6a53fe50db3ee6d8d69d
luciosacramento <91433765+luciosacramento@users.noreply.github.com>
2024-12-02 22:52:25 -0300

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -4,4 +4,6 @@ export const environment = {
   production: false,
   versao:versao,
   API_url: 'https://www2.ciberian.com.br/wp-json/custom/v1/',
+  chaveCaptcha: '6LennZAqAAAAAKeLQiNXHuncPWTfVgh0P70kWiDb',
+  chaveCaptchaSecreta: '6LennZAqAAAAAGbvI7kB09uwdCtKnBBZ0QjVccp0',
 };

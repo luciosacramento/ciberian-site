0769023ecbcc8339ff332323c407d3c4418646e7
lucio.sacramento <lucio.sacramento@tcm.ba.gov.br>
2025-01-17 14:27:57 -0300

    3. Ao clicar no link para ir ao site, a página está sendo carregada...

--- src/app/shared/dialogs/aguarde/aguarde.dialog.ts
+++ src/app/shared/dialogs/aguarde/aguarde.dialog.ts
@@ -1,9 +1,11 @@
 import { Component, Injectable } from '@angular/core';
+import { fadeDialogAnimations } from 'src/app/core/dialog.animations';
 
 
 @Component({
   templateUrl: './aguarde.dialog.html',
-  styleUrls: ['./aguarde.dialog.scss']
+  styleUrls: ['./aguarde.dialog.scss'],
+  animations: fadeDialogAnimations
 })
 
 //Dialog utilizada para dar feedbak imediato ao usuario, obrigando o mesmo a esperar a execução finalizar.
